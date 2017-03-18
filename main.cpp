3f920dd0758133336dc0109dba565d8c41985596
Ras0N <378907175@qq.com>
2017-03-18 23:15:52 +0800

    其他文件

---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+BOOL GetAccess() {
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tpk;
+    LUID Luid;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+    {
+        _tprintf(L"ACCESS DENYED!(%d)\n", GetLastError());
+        return FALSE;
+    }
+    {
+        if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &Luid))
+        {
+            _tprintf(L"Lookup Privilege Error!(%d)\n", GetLastError());
+            return FALSE;
+        }
+        tpk.PrivilegeCount = 1;
+        tpk.Privileges[0].Luid = Luid;
+        tpk.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+        if (!AdjustTokenPrivileges(hToken, FALSE, &tpk, sizeof(tpk), (PTOKEN_PRIVILEGES)NULL, 0))
+            _tprintf(L"Connot Get Access!\n");
+    }
+    return TRUE;
+}
+
+int main(int argc, char *argv[])
+{
+    if (!GetAccess())
+    {
+        _tprintf(L"Unable to get access.Is you running as Administrator?\n");
+        exit(1);
+    }
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ mainwindow.h
@@ -0,0 +1,97 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <QMainWindow>
+#include <QStatusBar>
+#include <QTextCodec>
+#include <QThread>
+#include <Qtime>
+#include <process.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <WinUser.h>
+#include <string.h>
+#include <Ras.h>
+#include <RasDlg.h>
+#include <raserror.h>
+#include <locale.h>
+#include <ctype.h>
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+#pragma execution_character_set("utf-8")
+#endif
+
+#pragma comment(lib, "rasapi32.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "User32.lib")
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    bool PPPoEinit();
+public slots:
+    void GetSaveConfirm(void);
+    void SetChangeFlag(void);
+    bool DoPPPoeDial(void);
+signals:
+    void RunThread();
+private:
+    Ui::MainWindow *ui;
+//  bool FindRAS();
+    bool InitSet();
+    bool bSaveConfirm;
+    void GetPPPoEUserName(TCHAR * const Uname,TCHAR * TName);
+    TCHAR szUsername[256]= {0,};
+    TCHAR szPPPname[256] = {0,};
+    TCHAR szPassword[256] = {0,};
+    bool savePhoneBook(TCHAR *const username,TCHAR * const password);
+    bool bChange = false;
+    void UpdateData(void);
+    void waitms(DWORD ms);
+protected:
+    bool eventFilter(QObject *target, QEvent *event);
+};
+class PPPoeThread:public QThread
+{
+    Q_OBJECT
+public:
+    PPPoeThread(){
+        FIN = FALSE;
+        STOP = TRUE;
+    }
+    //~PPPoeThread();
+    bool DoPPPdial(void);
+    bool GetThreadStat(void);
+    void GetID(TCHAR * const szPPPoename,TCHAR * const szPPPoePass);
+    DWORD getErrorCode(void);
+
+public slots:
+    void setRun();
+//    void start();
+//    void terminate();
+
+private:
+    BOOL GetPortName(TCHAR * lpBuffer);
+    HRASCONN DoPPP(TCHAR* UserName, TCHAR* Password);
+    TCHAR UserName[256];
+    TCHAR Password[256];
+    bool FIN;
+    bool STOP;
+    DWORD ucErrorcode;
+protected:
+    virtual void run();
+};
+
+#endif // MAINWINDOW_H
